de37c1f05fa43391eb3e3f58c93088bda1def2bb
suap-dev <suap.dev@gmail.com>
2023-01-17 16:54:00 +0100

    add .gitignore

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/suap/dev/oldRepos/showcase/school_register
+# Build directory: C:/suap/dev/oldRepos/showcase/school_register/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
